ed2482c40c992b677439799d7ceff13fdb684a18
Srinivas Tamada <srinivas@9lessons.info>
2019-03-29 23:04:27 -0400

    firebase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-circleci-firebase!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular CircleCI Firebase Pipeline Project');
   });
 });
